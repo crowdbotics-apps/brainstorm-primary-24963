496e4900a25e6f3005e633317530870bd024c55c
Crowdbotics <team@crowdbotics.com>
2021-03-11 01:05:04 +0000

    Update models

--- backend/brainstorm_primary_24963/wsgi.py
+++ backend/brainstorm_primary_24963/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brainstorm_primary_24963.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brainstorm_primary_24963.settings")
 
 application = get_wsgi_application()
